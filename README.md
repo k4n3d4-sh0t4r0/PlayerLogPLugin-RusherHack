76b882cbee224116e75ddab17a3f64025ec7b2b7
Kaneda Shotaro <k4n3d4_sh0t4r0@proton.me>
2025-07-15 13:57:01 +0200

    Added new options

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # RusherHack PlayerLog Plugin
 
-Rusherhack plugin that log all player encounters in a log file and display them in the chat (doesn't work on 2b2t for the moment)
+Rusherhack plugin that log all player encounters in a log file and display them in the chat
 
 # Features
 
@@ -22,10 +22,16 @@ Rusherhack plugin that log all player encounters in a log file and display them 
 #### NPC / Fake Player Filter
 - Fake players (e.g. on mini-game servers) are automatically ignored to avoid false positives.
 
+# Settings
+The Filter NPC option work by not logging player that has a ping of 0. Unfortunately 2b2t doesn't give the ping of other players connected and send a ping of 0 for everybody instead. So activating this option will disable the plugin on 2b2t and servers that send a ping of 0 for everybody
+
 # Screenshots
 
 #### Exemple of log in the chat
 ![PlayerLogChat](img/PlayerLogChat.png)
+
+#### Exemple of notification
+![PlayerLogNotification](img/PlayerLognotification.png)
 
 #### Windows module that show the log file
 ![PlayerLogWindow](img/PlayerLogWindows.png)
--- src/main/java/org/k4n3d4/PlayerLog/PlayerLogModule.java
+++ src/main/java/org/k4n3d4/PlayerLog/PlayerLogModule.java
@@ -1,8 +1,11 @@
 package org.k4n3d4.PlayerLog;
 
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
+import org.rusherhack.client.api.RusherHackAPI;
 import org.rusherhack.client.api.events.render.EventRender3D;
 import org.rusherhack.client.api.feature.module.ModuleCategory;
 import org.rusherhack.client.api.feature.module.ToggleableModule;
@@ -15,6 +18,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 import net.minecraft.client.Minecraft;
+import org.rusherhack.core.notification.NotificationType;
 import org.rusherhack.core.setting.BooleanSetting;
 
 import java.time.LocalDateTime;
@@ -24,16 +28,24 @@ import java.time.format.DateTimeFormatter;
 public class PlayerLogModule extends ToggleableModule {
 	private final BooleanSetting logInChat = new BooleanSetting("Log in chat", true);
 	private final BooleanSetting logInFile = new BooleanSetting("Log in file", true);
+	private final BooleanSetting logInNotification = new BooleanSetting("Notification", true);
+	private final BooleanSetting playASound = new BooleanSetting("Play a sound", true);
+	private final BooleanSetting ignoreFriend = new BooleanSetting("Ignore friend", true);
+	private final BooleanSetting filerNPC = new BooleanSetting("Filter NPC", "WARNING activating this option will disable the plugin on 2b2t and some servers", false);
 
 	// Log file path
 	private static final String logFilePath = "rusherhack/logs/player_log.txt";
 
 	public PlayerLogModule() {
-		super("PlayerLog", "Log all player encounters in a log file and display them in the chat", ModuleCategory.CLIENT);
+		super("PlayerLog", "Log all player encounters", ModuleCategory.CLIENT);
 
 		this.registerSettings(
 				logInChat,
-				logInFile
+				logInFile,
+				logInNotification,
+				playASound,
+				ignoreFriend,
+				filerNPC
 		);
 	}
 
@@ -56,19 +68,25 @@ public class PlayerLogModule extends ToggleableModule {
 					continue;
 				}
 
-				// Ignore fakeplayer by checking if it has a connection
-				if (Minecraft.getInstance().getConnection().getPlayerInfo(player.getUUID()) == null) {
-					continue;
-				}
+				if (filerNPC.getValue()) {
+					// Ignore fakeplayer by checking if it has a connection
+					if (Minecraft.getInstance().getConnection().getPlayerInfo(player.getUUID()) == null) {
+						continue;
+					}
 
-				// Ignore fakeplayer by checking if it has a ping equal to 0
-				int ping = Minecraft.getInstance().getConnection().getPlayerInfo(player.getUUID()).getLatency();
-				if (ping == 0) {
-					continue;
+					// Ignore fakeplayer by checking if it has a ping equal to 0
+					int ping = Minecraft.getInstance().getConnection().getPlayerInfo(player.getUUID()).getLatency();
+					if (ping == 0) {
+						continue;
+					}
 				}
 
 				UUID id = player.getUUID();
 				currentlyVisible.add(id);
+
+				if (ignoreFriend.getValue() && RusherHackAPI.getRelationManager().isFriend(player.getName().getString())) {
+					continue;
+				}
 
 				if (!playersInView.contains(id)) {
 					// Get date and time
@@ -95,8 +113,23 @@ public class PlayerLogModule extends ToggleableModule {
 					if (logInChat.getValue()) {
 						ChatUtils.print(logMessage);
 					}
+					if (logInNotification.getValue()) {
+						RusherHackAPI.getNotificationManager().send(NotificationType.INFO, logMessage);
+					}
 					if (logInFile.getValue()) {
 						logToFile(logMessage);
+					}
+					if (playASound.getValue()) {
+						mc.execute(() -> mc.level.playLocalSound(
+								mc.player.getX(),
+								mc.player.getY(),
+								mc.player.getZ(),
+								SoundEvents.EXPERIENCE_ORB_PICKUP,
+								SoundSource.PLAYERS,
+								1.0F,
+								1.0F,
+								false
+						));
 					}
 					playersInView.add(id);
 				}
